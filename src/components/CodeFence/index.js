d5c3a2b797c312541057e711abe2b5c9fa592e07
mateusbra <suehtamzarb@gmail.com>
2022-04-18 15:13:56 -0300

    testing panGestureHandler

--- src/components/CodeFence/index.js
+++ src/components/CodeFence/index.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
 import codeFencePropTypes from './codeFencePropTypes';
-
+//import { PanGestureHandler } from 'react-native-gesture-handler';
 class CodeFence extends React.Component {
     constructor(props) {
         super(props);
-
-        this.onScroll = this.onScroll.bind(this);
-    }
-
-    componentDidMount() {
-        this.scrollViewRef.addEventListener('wheel', this.onScroll);
-    }
-
-    componentWillUnmount() {
-        this.scrollViewRef.removeEventListener('wheel', this.onScroll);
-    }
-
-    onScroll(e) {
-        this.scrollViewRef.scrollLeft += e.deltaX;
     }
 
     render() {
         const TDefaultRenderer = this.props.TDefaultRenderer;
         return (
-            <ScrollView ref={el => this.scrollViewRef = el} horizontal>
+            <ScrollView horizontal>
                 <TDefaultRenderer
                     // eslint-disable-next-line react/jsx-props-no-spreading
                     {...this.props.defaultRendererProps}
